aa0a582c2a8b3c0464e3d0c57d7aba4a45b9ab44
DaejonFerrao <daejonferrao@gmail.com>
2023-12-18 15:28:07 +0000

    made changes to events

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import React, {useState} from 'react';
 
-// import Categories from './Components/Categories/Categories';
-// import LunchMenu from './Components/LunchMenu/LunchMenu';
-
-
+import Menu from './Components/Menu/Menu';
 
 function App() {
-  const [menuItems, setMmnuItems] = useState(item);
-  const [activeCategory, setActiveCategory] = useState("");
-  const [Categories, setCategories] = useState()
   return(
     <div className="App">
-
+      <Menu />
     </div>
   );
 }
---
+++ src/Components/Menu/Menu.js
@@ -0,0 +1,20 @@
+import "./Menu.css"
+
+const Menu = () => {
+    return(
+        <section className="features-box">
+            <div className="container">
+                <div className="intro">
+                    <h2 className="center-text">Menu</h2>
+                    <p className="center-text">
+                        This is where the text will do once
+                        there is text to be displayed. 
+                    </p>
+                </div>
+                <div className="row features"></div>
+            </div>
+        </section>
+    );
+};
+
+export default Menu;
