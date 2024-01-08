1f9939bb4372a604942c968d2f0c04491ef68c11
DaejonFerrao <daejonferrao@gmail.com>
2024-01-08 20:41:47 +0000

    enquiry update

--- src/App.js
+++ src/App.js
@@ -2,22 +2,20 @@ import React, { useState } from 'react';
 
 // import Menu from './Components/Menu/Menu';
 // import Item from './Components/Item/Item';
-// import Events from './Components/Events/Events';
-// import Enquiry from './Components/Enquiry/Enquiry';
-// import ContactUs from './Components/ContactUs/ContactUs';
-// import Intro from './Components/Intro/Intro';
+import Events from './Components/Events/Events';
+import Enquiry from './Components/Enquiry/Enquiry';
+import Intro from './Components/Intro/Intro';
 import NavBar from "./Components/NavBar/NavBar"
 
 
 function App() {
   return (
     <div className="App">
-      {/* <Intro /> */}
       <NavBar />
-      {/* <ContactUs /> */}
+      <Intro />
+      {/* <Events />  
+      <Enquiry />  */}
       {/* <Menu /> */}
-      {/* <Events />  */}
-      {/* <Enquiry />  */}
       {/* <Item /> */}
     </div>
   );
--- src/Components/Intro/Intro.js
+++ src/Components/Intro/Intro.js
@@ -1,9 +1,14 @@
-import "./Intro.css"
-import background from "../../Assets/callymunchyhome.jpg";
+import "./Intro.css";
+// import background from "../../Assets/callumunchyhome.jpg";
 
 const Intro = () => {
     return (
-        <img className="background"></img>
+   <><div class="hero-image" /> 
+   <img src="https://images.squarespace-cdn.com/content/v1/64e3926eb597a92d578672bc/1ef93da8-4216-40db-abee-2974144808d7/Cally+Munchy+X+Abei-02.jpg?format=1500w" /> 
+   <div class="hero-text" />
+   <h1>CALLY MUNCHY</h1>
+   <p>Welcome</p>
+   <button>Explore More</button></>
     );
 };
 
